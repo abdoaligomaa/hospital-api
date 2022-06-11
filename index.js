0dc3200830d5ccaeccb95680ffd618d88e4e0464
abdoaligomaa <abdoaligomaa107@gmail.com>
2022-06-11 08:27:39 +0200

    migrate data to db and change in models

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express=require('express')
+// import { PrismaClient } from '@prisma/client'
+const {PrismaClient}=require("@prisma/client")
+const prisma = new PrismaClient()
 
 
 const port=5000
@@ -7,6 +10,10 @@ const app=express()
 
 app.get('/',(req,res)=>{
     res.send('welcome in my project')
+})
+
+app.get('/creatDoctor',async(req,res)=>{
+    const doctor=await prisma
 })
 
 app.listen(port,console.log('server is running on port '+port))
