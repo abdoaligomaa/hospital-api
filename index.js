ded89b2ca0d5434029b1a361490b65acb0084a07
abdoaligomaa <abdoaligomaa107@gmail.com>
2022-06-11 09:33:45 +0200

    creat new department with patients

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
-const express=require('express')
+const express = require("express");
 // import { PrismaClient } from '@prisma/client'
-const {PrismaClient}=require("@prisma/client")
-const prisma = new PrismaClient()
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
+const port = 5000;
 
-const port=5000
+const app = express();
 
-const app=express()
+app.get("/", async (req, res) => {
+  const patients = await prisma.patient.findMany({});
+  res.json(patients);
+});
+app.post("/creat", async (req, res) => {
+  const department = await prisma.department.create({
+    data: {
+      name: "Dermatology",
+      patients: {
+        create: [
+          {
+            name: "abdo ali gomaa",
+            email: "abdo@gmail.com",
+            password: "abdo password",
+          },
+          {
+            name: "abdo ali gomaa2",
+            email: "abdo2@gmail.com",
+            password: "abdo password2",
+          },
+        ],
+      },
+    },
+  });
+  res.json(department   );
+});
 
-app.get('/',async(req,res)=>{
-    const patients=await prisma.patient.findMany()
-    res.json(patients)
-})
-app.post('/',async(req,res)=>{
-    const patients=await prisma.patient.findMany()
-    res.json(patients)
-})
-app.get('/creatDoctor',async(req,res)=>{
-    const doctor=await prisma
-})
-
-app.listen(port,console.log('server is running on port '+port))
+app.listen(port, console.log("server is running on port " + port));
