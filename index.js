8f3151c0c5d84ee26d2bc41b76329e597bb7a483
abdoaligomaa <abdoaligomaa107@gmail.com>
2022-06-21 08:15:06 +0200

    put the structure of the api

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log('hellow world')
+const {PrismaClient}=require('@prisma/client')
+const prisma=new PrismaClient()
+
+const express=require('express')
+const app=express()
+
+
+// create department
+//get all department 
+// get deparment info 
+// update deparment info 
+
+// create student
+//get all student in deparment 
+//get all student for teacher 
+//get all student in cours 
+// get student info 
+// update student info
+
+// create teacher
+//get all teacher in deparment 
+// get teacher info 
+// update teacher info
+ 
+
+// create coure
+// get all coures
+// get all course in the deparment
+// get all course for one student
+// update coures
+// delete course
+
+app.listen('3000',()=>console.log('server is listen to port 3000'))
