1f95b2096e99b5b6af585702d91ea16337afa9bc
abdoaligomaa <abdoaligomaa107@gmail.com>
2022-06-21 08:57:43 +0200

    create department and get all department

--- index.js
+++ index.js
@@ -1,27 +1,61 @@
-const {PrismaClient}=require('@prisma/client')
-const prisma=new PrismaClient()
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
-const express=require('express')
-const app=express()
-
+const express = require("express");
+const app = express();
 
 // create department
-//get all department 
-// get deparment info 
-// update deparment info 
+app.post("/createDepartment", async (req, res) => {
+  try {
+    const deparment = await prisma.department.create({
+      data: {
+        name: "Secondary",
+        description: "this is for student from 13to 16 years",
+      },
+    });
+    res.json({ deparment });
+  } catch (error) {
+    res.json({ error });
+    
+  }
+});
+//get all department
+app.get("/allDepartment", async (req, res) => {
+     try {
+       const deparments = await prisma.department.findMany({
+         select: {
+           id: true,
+           name: true,
+           description: true,
+        //    students:true
+        createdAt:true
+
+         },
+       });
+       res.json({ deparments });
+     } catch (error) {
+        console.log(error)
+       res.json({ error });
+     }
+
+    
+})
+
+
+// get deparment info
+// update deparment info
 
 // create student
-//get all student in deparment 
-//get all student for teacher 
-//get all student in cours 
-// get student info 
+//get all student in deparment
+//get all student for teacher
+//get all student in cours
+// get student info
 // update student info
 
 // create teacher
-//get all teacher in deparment 
-// get teacher info 
+//get all teacher in deparment
+// get teacher info
 // update teacher info
- 
 
 // create coure
 // get all coures
@@ -30,4 +64,4 @@ const app=express()
 // update coures
 // delete course
 
-app.listen('3000',()=>console.log('server is listen to port 3000'))
+app.listen("3000", () => console.log("server is listen to port 3000"));
